ec32801b244d95b6b7b9cf33bcbca78fc3527719
Ajoy Bharath <ajoy.bharath@oracle.com>
2018-09-02 14:25:42 +0530

    Automation support

---
+++ project_fab/fabfile.py
@@ -0,0 +1,8 @@
+from fabric.api import run, env, settings
+from fabric.operations import sudo, local
+
+env.hosts = open('hosts_file', 'r').readlines()
+env.password = 'XXXXX'
+def instocla():
+        with settings(prompts={"Password: " : "XXXXX"}):
+        run("sudo  userad testuser")
